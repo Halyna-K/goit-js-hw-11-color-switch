8086ed82850646f65d5c5740b0183db34560cbb7
Halyna-K <hkzlvs@gmail.com>
2021-08-22 16:47:25 +0300

    Create switcher

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ const colors = [
     '#009688',
     '#795548',
   ];
-
+const refs = {
+start: document.querySelector('[data-action="start"]'),
+stop: document.querySelector('[data-action="stop"]')
+  }
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
+let intervalId = null;
+
+refs.start.addEventListener('click', randomStart);
+refs.stop.addEventListener('click', randomStop);
+
+function randomStart () {
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, 1000);
+    refs.start.disabled = true;
+};
+
+function randomStop() {
+    clearInterval(intervalId);
+    refs.start.disabled = false;
+}
